702788fc69c937e4125481c0234c32b653d0a1c3
Juwar <muhammad.juwarnoo@gmail.com>
2025-02-15 09:34:26 +0700

    Fix error build

--- src/components/meter-readings/editable-meter.tsx
+++ src/components/meter-readings/editable-meter.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Pencil, Save, CircleX } from "lucide-react";
+import { Pencil, Save } from "lucide-react";
 
 interface EditableMeterProps {
   readingId: number;
